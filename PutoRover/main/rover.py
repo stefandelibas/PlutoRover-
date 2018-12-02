1d3d74b0a1b8e1148318d5c6c911e8b505ecf5c8
Stefan Delibas <delibas.stefan@gmail.com>
2018-12-02 19:03:17 +0200

    simple test for forward

--- PutoRover/main/rover.py
+++ PutoRover/main/rover.py
@@ -1,8 +1,27 @@
 
 
 class Rover:
-    def __init__(self):
-        pass
+    def __init__(self, x=0, y=0, direction='N', grid_width=100, grid_height=100):
+        self.n = grid_height
+        self.m = grid_width
+        self.x = x
+        self.y = y
+        self.d = direction
+
+    def set_position(self, x, y, d):
+        # whenever we want to reinitialize the position of the rover(typically in testing)
+        self.x = x
+        self.y = y
+        self.d = d
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def getD(self):
+        return self.d
 
     def backward(self):
         pass
--- PutoRover/test/AllTests.py
+++ PutoRover/test/AllTests.py
@@ -3,7 +3,33 @@ from main.rover import Rover
 
 class MyTestCase(unittest.TestCase):
     def test_forward(self):
-        pass
+        self.test_forward_north()
+        self.test_forward_east()
+
+    def test_forward_north(self):
+        rover = Rover(5, 5, 'N', 10, 10)
+        rover.forward()
+        self.assertEqual(rover.getX(), 5)
+        self.assertEqual(rover.getY(), 6)
+
+    def test_forward_east(self):
+        rover = Rover(5, 5, 'E', 10, 10)
+        rover.forward()
+        self.assertEqual(rover.getX(), 6)
+        self.assertEqual(rover.getY(), 5)
+
+    def test_forward_south(self):
+        rover = Rover(5, 5, 'S', 10, 10)
+        rover.forward()
+        self.assertEqual(rover.getX(), 5)
+        self.assertEqual(rover.getY(), 4)
+
+    def test_forward_west(self):
+        rover = Rover(5, 5, 'W', 10, 10)
+        rover.forward()
+        self.assertEqual(rover.getX(), 4)
+        self.assertEqual(rover.getY(), 5)
+
 
     def test_backward(self):
         pass
